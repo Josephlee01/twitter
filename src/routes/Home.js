53a4379a028e339a53cabbdacfae4fc89fe8c88d
josephlee01 <bluewings113@gmail.com>
2021-09-17 15:43:08 +0900

    #3.2 Getting Twts

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { db } from "../fbase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 
 const Home = () => {
   const [tweet, setTweet] = useState("");
+  const [tweets, setTweets] = useState([]);
+  const getTweets = async () => {
+    const dbTweets = await getDocs(collection(db, "tweets"));
+    dbTweets.forEach((doc) => {
+      const tweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setTweets((prev) => [...prev, tweetObj]);
+    });
+  };
+  useEffect(() => {
+    getTweets();
+  }, []);
+
   const onSubmit = async (e) => {
     e.preventDefault();
     await addDoc(collection(db, "tweets"), {
       tweet,
       publishedDate: Date.now(),
     });
-    setTweet("")
+    setTweet("");
   };
   const onChange = (e) => {
     setTweet(e.target.value);
@@ -27,6 +42,13 @@ const Home = () => {
         />
         <input type="submit" value="Tweet" />
       </form>
+      <div>
+        {tweets.map((t) => (
+          <div key={t.id}>
+            <h4>{t.tweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
